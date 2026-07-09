8b051fc62f594e0e52933e99ecedffcdb5d53f14
Ömer Faruk Altun <altunomerfarukk@gmail.com>
2026-07-09 10:12:08 +0300

    Start

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameCore;
+
+
+namespace OnlineKitap.data
+{
+    public class AppDbContext : DbContext
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
